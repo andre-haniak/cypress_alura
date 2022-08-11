1db45f89ddd198600d4ba9ff258e5ea575ba303c
andre-haniak <assainks@gmail.com>
2022-08-11 14:44:28 -0300

    added and configured mochawesome in cypress.config

--- cypress.config.js
+++ cypress.config.js
@@ -4,14 +4,16 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-        "reporter": "mochawesome",
-        "reporterOptions": {
-        "reportDir": "cypress/report/mochawesome-report",
-        "overwrite": true,
-        "html": true,
-        "json": false,
-        "timestamp": "mmddyyyy_HHMMss"
-        }    
+    },
+      "reporter": "mochawesome",
+      "reporterOptions": {
+      "reportDir": "cypress/report/mochawesome-report",
+      "overwrite": true,
+      "html": true,
+      "json": false,
+      "timestamp": "mmddyyyy_HHMMss"    
     },
   },
 });
+
+
