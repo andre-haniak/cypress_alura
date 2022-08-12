78d57560677041dc33f9c7a8f927ba07aed41aaa
andre-haniak <assainks@gmail.com>
2022-08-12 11:50:22 -0300

    implemented login via API

--- cypress/e2e/3-AluraPic/api-alurapic.cy.js
+++ cypress/e2e/3-AluraPic/api-alurapic.cy.js
@@ -11,4 +11,17 @@ describe('Buscar fotos e dados', () => {
             expect(res.body[0].description).to.be.equal('Farol iluminado')
         })
     });
+
+    it.only('Fazer login', () => {
+        cy.request({
+            method: 'POST',
+            url: 'https://apialurapic.herokuapp.com/user/login',
+            body: Cypress.env()
+        }).then((res) => {
+            expect(res.status).to.be.equal(200)
+            expect(res.body).is.not.empty
+            expect(res.body).to.have.property('id')
+            expect(res.body.id).to.be.equal(6)
+        })
+    });
 })
