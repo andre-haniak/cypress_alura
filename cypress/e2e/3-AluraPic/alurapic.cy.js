92fea9cfbdfc8bb6bbf090ac58096775eada3f8f
andre-haniak <assainks@gmail.com>
2022-08-12 09:29:22 -0300

    added custom commands for running tests

--- cypress/e2e/3-AluraPic/alurapic.cy.js
+++ cypress/e2e/3-AluraPic/alurapic.cy.js
@@ -21,14 +21,12 @@ describe('Login e registro de usuários alura pic', () =>{
         cy.contains('ap-vmessage', 'Invalid e-mail').should('be.visible')
     });
 
-
     it('Verifica mensagens de senha com menos de 8 caracteres', () => {
         cy.contains('a', 'Register now').click()
         cy.get('input[formcontrolname="password"]').type('12345')
         cy.contains('button', 'Register').click()
         cy.contains('ap-vmessage', 'Mininum length is 8').should('be.visible')
     });
-
 
     it('Verifica mensagem de usuário está com letra maiúscula', () => {
         cy.contains('a', 'Register now').click()
@@ -37,4 +35,15 @@ describe('Login e registro de usuários alura pic', () =>{
         cy.contains('ap-vmessage', 'Must be lower case').should('be.visible')
     });
 
+    it.only('fazer login de usuário válido', () => {
+        cy.login('assainks', '12345678')
+        cy.contains('a', '(Logout)').should('be.visible')
+    });
+
+    it.only('fazer login de usuário inválido', () => {
+        cy.login('assainks', '123456')
+        cy.on('window:alert', (str) => {
+            expect(str).to.equal('Invalid user name or password')
+        })
+    });
 })
